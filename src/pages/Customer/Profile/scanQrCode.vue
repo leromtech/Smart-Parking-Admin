5f54db5909ce8704ecc9795cbc096f8637c9d015
Laldinpuia <dinahnamte@gmail.com>
2024-11-18 15:12:58 +0530

    qr code test na

--- src/pages/Customer/Profile/scanQrCode.vue
+++ src/pages/Customer/Profile/scanQrCode.vue
@@ -1,14 +1,14 @@
 <template>
-    <div class="w-full h-dvh flex p-8 pt-16 justify-center bg-[#0B0B0B]">
-        <div class="qr-container border-2">
-            <qrcode-stream :camera="{
-                facingMode: { exact: 'environment' },
-                width: 1280,
-                height: 720
-            }" @decode="onDecode" @init="onInit">
+    <div class="w-full h-dvh flex p-8 pt-16 justify-center items-center flex-col bg-[#0B0B0B]">
+        <div class="qr-container relative">
+            <qrcode-stream @decode="onDecode" @init="onInit">
+                <div class="corner top-left"></div>
+                <div class="corner top-right"></div>
+                <div class="corner bottom-left"></div>
+                <div class="corner bottom-right"></div>
             </qrcode-stream>
         </div>
-        <p v-if="qrCodeData">QR Code Data: {{ qrCodeData }}</p>
+        <p class="text-white">QR Code Data: {{ qrCodeData }}</p>
     </div>
 </template>
 
@@ -18,22 +18,19 @@ import { QrcodeStream } from 'vue-qrcode-reader';
 
 const qrCodeData = ref(null);
 
-const onDecode = (data) => {
-    console.log('Decoded QR Code Data:', data);
+const onDecode = async (data) => {
     qrCodeData.value = data;
-    sendQrCodeData();
+    await sendQrCodeData();
 };
 
 const onInit = (promise) => {
-    promise
-        .then(() => console.log('Camera initialized successfully'))
-        .catch(error => {
-            console.error('Could not initialize camera', error);
-        });
+    promise.catch(error => {
+        console.error('Could not initialize camera', error);
+    });
 };
 
-const sendQrCodeData = () => {
-    console.log('Sending QR Code Data:', qrCodeData.value);
+const sendQrCodeData = async () => {
+    console.log(qrCodeData.value)
 };
 </script>
 
@@ -41,5 +38,42 @@ const sendQrCodeData = () => {
 .qr-container {
     width: 300px;
     height: 300px;
+    position: relative;
+}
+
+.corner {
+    width: 60px;
+    height: 60px;
+    border: 6px solid #ffffff;
+    /* Adjust color as needed */
+    position: absolute;
+}
+
+.top-left {
+    top: 0;
+    left: 0;
+    border-right: none;
+    border-bottom: none;
+}
+
+.top-right {
+    top: 0;
+    right: 0;
+    border-left: none;
+    border-bottom: none;
+}
+
+.bottom-left {
+    bottom: 0;
+    left: 0;
+    border-right: none;
+    border-top: none;
+}
+
+.bottom-right {
+    bottom: 0;
+    right: 0;
+    border-left: none;
+    border-top: none;
 }
 </style>
