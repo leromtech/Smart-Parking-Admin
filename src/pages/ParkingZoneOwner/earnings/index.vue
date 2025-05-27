9ff7c3d09f4f4ed2b14757b597f49d1a6c8f2511
DinaHnamte <dinahnamte@gmail.com>
2025-05-27 09:35:03 +0530

    minor ui changes

--- src/pages/ParkingZoneOwner/earnings/index.vue
+++ src/pages/ParkingZoneOwner/earnings/index.vue
@@ -4,10 +4,14 @@
             <div class="flex flex-row gap-8">
                 <div class="flex flex-row gap-4 w-full">
                     <IftaLabel>
-                        <DatePicker v-model="monthFilter" showIcon fluid iconDisplay="input" view="date"
-                            dateFormat="MM yy dd" />
-                        <label for="date">Month</label>
+                        <DatePicker v-model="monthFilter" selectionMode="range" :manualInput="false"
+                            class="w-full md:w-40" dateFormat="MM yy dd" />
+                        <label for="date">Date filter</label>
                     </IftaLabel>
+
+                    <Select v-model="filters.status" class="md:w-80 w-full" placeholder="Select Status"
+                        :options="statusOptions" optionLabel="label" optionValue="value">
+                    </Select>
                 </div>
                 <div class="flex flex-col items-end justify-end w-full">
                     <span class="font-semibold">
@@ -72,7 +76,8 @@ import { useParkingZone } from '../../../scripts/parkingZone';
 
 const { parking_zone } = useParkingZone()
 const filters = ref({
-    search: ''
+    search: '',
+    status: ''
 })
 
 const totals = ref()
@@ -81,17 +86,32 @@ const toast = useToast()
 
 const loading = ref(false)
 
-const monthFilter = ref(new Date())
+const monthFilter = ref([
+    new Date(),
+])
 
 const payments = ref([])
 
 const status = ref('')
 
+const statusOptions = ref([
+    { label: 'All', value: '' },
+    { label: 'Paid', value: 'paid' },
+    { label: 'Pending', value: 'pending' }
+])
+
 const getEarnings = async () => {
     loading.value = true
     try {
+        let dateFilter = {
+            start: null,
+            end: null
+        }
+        if (monthFilter.value[1]) {
+            dateFilter.end = monthFilter.value[1].toLocaleDateString('en-GB', { month: 'numeric', year: 'numeric', day: 'numeric' }).replaceAll('/', '-');
+        }
         const month = monthFilter.value.toLocaleDateString('en-GB', { month: 'numeric', year: 'numeric', day: 'numeric' }).replaceAll('/', '-');
-        const { data } = await api.get('earnings', { params: { parking_zone_id: parking_zone.value.id, month } });
+        const { data } = await api.get('earnings', { params: { parking_zone_id: parking_zone.value.id, month, filters: filters.value } });
         payments.value = data.payments;
         status.value = data.status
         totals.value = data.totals
@@ -101,6 +121,10 @@ const getEarnings = async () => {
         console.error('Error fetching earnings:', error);
     }
 };
+
+watch(filters, async (newVal) => {
+    await getEarnings()
+}, { deep: true })
 
 watch(monthFilter, async (newVal) => {
     await getEarnings()
